aa5978f2051195deea37e8201ba018cb5d1af30c
muyeqinshimingyue <1574862254@qq.com>
2017-07-21 17:23:41 +0800

    测试http服务器，模块化路由

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'); // 导入express模块
+var router = express.Router(); // 创建路由对象
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+/*  拦截所有请求  */
+router.all("*",function (req,res,next) {
+    console.log(" 访问用户请求 ");
+
+    next();  // 表示继续走下一个拦截器
+})
+
+router.get('/list', function(req, res, next) {
+    res.send('获取用户列表');
+})
 
 module.exports = router;
