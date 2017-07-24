d5b94b470ce77fca5204f3a99d87411769baf52b
muyeqinshimingyue <1574862254@qq.com>
2017-07-24 12:25:24 +0800

    测试http服务器的三大对象  route  request  response

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,71 @@
+/**
+ *  测试http服务器的三大对象  route  request  response
+ * @type {*|createApplication}
+ */
 var express = require('express'); // 导入express模块
 var router = express.Router(); // 创建路由对象
 
 /*  拦截所有请求  */
 router.all("*",function (req,res,next) {
-    console.log(" 访问用户请求 ");
+
+    console.log(" *****************   拦截所有访问用户请求  *********************** ");
+    //   请求的URL  req.url 与 req.path 区别：req.url  如果请求的Url带有参数，它会获取到参数信息，但是path不会
+    console.log("请求的URL访问地址:  " + req.url );        //  请求的URL /list?id=100001
+    console.log("请求的URL方法： " +  req.method );       //   请求的URL方法
+    console.log("请求的URL是： " +  req.path );          //    请求的URL  /list
+    console.log("获取路由的基础路径：" +  req.baseUrl );  // 获取路由的基础路径：/user
+
+
+    console.log( "\n" );
+    console.log( " 请求头部信息如下：" );
+    console.log( req.headers );  // 请求头部信息 ，该信息是一个对象
+    console.log( "" );
+    console.log( " 请求头中‘user-agent’ 信息： " + req.headers["user-agent"] );  // 请求头中的某个信息
+    console.log( " 请求头中‘user-agent’ 信息： " + req.get("user-agent") );       // 请求头中的某个信息
+    console.log(  " 请求头中‘ host’ 信息： " + req.headers.host );                 // 请求头中的某个信息
+    console.log(  " 请求头中‘ host’ 信息： " + req.header("host") );               // 请求头中的某个信息
+
+
+    console.log( req.query );  // 获取get方式提交的查询参数 是一个对象  { id: '100001' }
+    console.log("查询参数中的ID的值:   " + req.query.id );  // 获取提交的查询参数 是一个对象  { id: '100001' }
+
+    console.log( req.body );   // 获取post方式提交的参数对象  { name: '1232', as: '328' }
+    console.log( "post参数中的name的值:   " + req.body.name ); // 获取post方式中的某个参数
+
 
     next();  // 表示继续走下一个拦截器
 })
-
+/*  获取用户列表  */
 router.get('/list', function(req, res, next) {
-    res.send('获取用户列表');
+    res.send('get 方式  获取用户列表');
 })
 
-module.exports = router;
+/*  获取用户列表  */
+router.post('/list', function(req, res, next) {
+    res.send('post 方式 获取用户列表');
+})
+
+/*  rest风格  获取URL中的参数  */
+router.all('/get/:id', function(req, res, next) {
+    res.send(' 获取 rest风格  用户的信息,用户ID是'+ req.params.id);
+})
+
+/*  res对象设置响应吗   */
+router.get('/fb', function(req, res) {
+    res.status(403).send('您没有访问当前资源的权限！');
+})
+
+/*  res对象 发送js文件   */
+router.get('/js', function(req, res) {
+    res.contentType("application/javascript"); // 设置文件格式为 js
+    res.sendFile( "index.js",{root:"../routes"} );  // 发送文件，设置文件名，及路径
+})
+
+/*  res对象 发送json 对象   */
+router.get('/json', function(req, res) {
+    res.json({"name":123213,"age":12});
+})
+
+
+
+module.exports = router;   // 返回该路由
--- test/testHttpServer.js
+++ test/testHttpServer.js
@@ -6,23 +6,24 @@ var  express = require("express");   // 导入第三方核心模块  express
 var bodyParser = require("body-parser");    // 导入参数 转换对象
 var path = require("path");
 
-
 var app = express();   //  创建应用服务器  调用 createApplication 方法
-
 
 /*  该路由的作用是拦截所用请求，对其中的post方式提交的Json参数进行解析，将其还原成Json对象  */
 app.use( bodyParser.json() );
 /*  该路由的作用是拦截所有请求，对其中的以urlencoded方式提交参数的请求进行解析  */
 app.use( bodyParser.urlencoded( { extended:false} ) );
 
-/*  该路由作用是设置静态文件所在位置 */
+/*  该路由作用是设置静态文件所在位置 __dirname 被执行的 js 所在文件夹的绝对路径  */
 app.use( express.static( path.join(__dirname, 'public') )  );
 
 /*  使用模块化路由配置  注意：使用 app.use  来调用 */
 app.use( "/user" , require("../routes/users")  );
 
+/*  路由：拦截所有/ 请求，也可以使用app.get   app.post  拦截  */
 app.all("/",function (req,res) {
-    res.send("hello") ;
+    console.log( req.path );
+    console.log( req.baseUrl );
+    res.send("hello,欢迎访问。。。。") ;
 });
 
 
@@ -32,11 +33,11 @@ server.on( "listening",function () {   // 监听服务器
     console.log("服务器启动，地址 http://localhost:%s" , getPort() );
 } );
 
-/***    获取端口号    */
+/***    获取端口号    */监听事件
 function getPort(){
     var port = process.env.PORT;
     if( port && isNaN( port ) ){
         port = parseInt( port );
-        return  (port > 0 && port < 65536) ? port:85 ;
-    }else  return  85;
+        return  (port > 0 && port < 65536) ? port:3000 ;
+    }else  return  3000;
 }
